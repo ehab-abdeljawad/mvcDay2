d0e5dcf90975f14a4bd01fad59aec88c8ad4897d
ehab-abdeljawad <ehababdeljawad1120@gmail.com>
2024-02-18 14:04:40 +0200

    project with projects color

---
+++ mvcDay2/Controllers/workforController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using mvcDay2.Migrations;
+using mvcDay2.Models;
+using mvcDay2.viewmodel;
+
+namespace mvcDay2.Controllers
+{
+   
+    public class workforController : Controller
+	{
+		banhacontext context = new banhacontext();
+		public IActionResult showhours()
+		{
+			var hours = context.work_Fors.Include(w=>w.Project).Include(w=>w.employee).ToList();
+            var viewproject = hours.Select(h =>
+            {
+                var color = h.Houres > 50 ? "green" : "red";
+                return new WorkForViewModel { Color = color, fname = h.employee.fname,lname = h.employee.lname,hours=h.Houres };
+            });
+
+           ViewBag.hours = viewproject;
+
+            return View();
+		}
+
+
+        public IActionResult detailshours(int id)
+        {
+
+            var hours = context.work_Fors.Include(w => w.Project).Include(w => w.employee).Where(e=>e.employee.Ssn == id).ToList();
+            List<string> colors = new List<string>();
+            string name = "";
+            foreach (var h in hours)
+            {
+                name = h.employee.fname + " "+h.employee.lname;
+                if (h.Houres > 50)
+                {
+                    colors.Add("green");
+                }
+                else
+                {
+                    colors.Add("red");
+
+                }
+            }
+            ;
+            ViewBag.name = name;
+            ViewBag.hours = colors;
+            return View(hours);
+
+        }
+
+
+	}
+}
---
+++ mvcDay2/viewmodel/WorkForViewModel.cs
@@ -0,0 +1,12 @@
+﻿using mvcDay2.Models;
+
+namespace mvcDay2.viewmodel
+{
+    public class WorkForViewModel
+    {
+        public string fname{ get; set; }
+        public string lname { get; set; }
+        public string Color { get; set; }
+        public int hours { get; set; }
+    }
+}
